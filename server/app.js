e6732a4a01e19ec580341009a910a0ca351ea12f
Harsha Mangipudi <mvvsharsha06@gmail.com>
2026-07-17 17:34:14 +0530

    add JSON middleware and API testing

--- server/app.js
+++ server/app.js
@@ -4,15 +4,32 @@ const express = require("express");
 //create application through express
 const app = express();
 
-//handle route
+//middleware to breakdown request that express understands or parses into a javascript object 
+app.use(express.json());
+
+//home route
 app.get("/", (req,res)=> 
    {
  res.send("This is EuphoricALLY OTT Backend");
    }
 );
 
+//temporary test route
+app.post("/test", (req,res)=>
+    {
+        console.log(req.body);
+    
+    res.json({
+        message : "Data received successfully",
+        receivedData : req.body
+    });
+
+});
+
 //export the app
 module.exports = app;
+
+
 
 
 
